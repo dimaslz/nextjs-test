030aa0119e58976aea8e76a78e5c244a3027bf32
dimaslz <me@dimaslz.io>
2023-04-18 13:26:47 +0200

    fix: add redirection by country by IP (vervel)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
+import localeByCountry from "./utils/localeByCountry.util";
 
 const PUBLIC_FILE = /\.(.*)$/
 
 export async function middleware(req: NextRequest) {
-	console.log("GEO", req.geo);
-
-	console.log("Headers", req.headers)
-
 	if (
 		req.nextUrl.pathname.startsWith('/_next') ||
 		req.nextUrl.pathname.includes('/api/') ||
 		PUBLIC_FILE.test(req.nextUrl.pathname)
 	) {
 		return
+	}
+
+	if (req.geo?.country) {
+		const locale = localeByCountry(req.geo?.country);
+
+		return NextResponse.redirect(
+			new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+		)
 	}
 
 	if (req.nextUrl.locale === 'default') {
---
+++ src/utils/localeByCountry.util.ts
@@ -0,0 +1,11 @@
+const localeByCountry = (country: string) => {
+	const Default = "es-ES";
+
+	return {
+		"ES": Default,
+		"UK": "en-GB",
+		"FR": "fr-FR",
+	}[country] || Default;
+};
+
+export default localeByCountry;

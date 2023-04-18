caa867a5e6133a7f3e422c2fdf46ae3a1841f8bb
dimaslz <me@dimaslz.io>
2023-04-18 13:58:11 +0200

    chore: console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,20 +16,20 @@ export async function middleware(req: NextRequest) {
 	console.log("Country", req.geo?.country);
 	console.log("Locale", req.nextUrl.locale);
 
-	if (req.geo?.country) {
-		const locale = localeByCountry(req.geo?.country);
-
-		console.log("Locale", locale, req.nextUrl.locale);
-		if (req.nextUrl.locale === locale) return;
-
-		return NextResponse.redirect(
-			new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-		)
-	}
-
 	if (req.nextUrl.locale === 'default') {
+		if (req.geo?.country) {
+			const locale = localeByCountry(req.geo?.country);
+
+			return NextResponse.redirect(
+				new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+			)
+		}
 		return NextResponse.redirect(
 			new URL(`/es-ES${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
 		)
 	}
+
+	return NextResponse.rewrite(
+		new URL(`${req.nextUrl.locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+	)
 }

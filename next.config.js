5570e3b261d1003c0595de1b90c7795d49fe9a8a
dimaslz <me@dimaslz.io>
2023-04-18 13:03:47 +0200

    feat: add locales

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['default', 'es-ES', 'fr-FR', 'en-GB'],
+    defaultLocale: 'default',
+    localeDetection: false,
+  }
 }
 
 module.exports = nextConfig
